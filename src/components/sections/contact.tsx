1a78588b29c14247f06fb587a67c30cd34a6f3ef
learnstack129 <learnstack129@gmail.com>
2025-12-25 13:23:48 +0530

    Update contact.tsx

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -71,7 +71,7 @@ export default function Contact() {
               </div>
             </div>
             <div className="mt-8 rounded-lg bg-muted h-64 flex items-center justify-center">
-              <p className="text-muted-foreground">Google Maps Placeholder</p>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3784.5303037092363!2d73.93305077519015!3d18.459623282622204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDI3JzM0LjYiTiA3M8KwNTYnMDguMyJF!5e0!3m2!1sen!2sin!4v1766649159936!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
           </AnimateOnScroll>
           <AnimateOnScroll animation="slide-in-right" className="lg:col-span-3">
